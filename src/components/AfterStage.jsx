54f9c459b6d840f00466ec488fb648c1bc9302cb
gjh <guyjohnhilton@gmail.com>
2025-09-27 20:31:51 +0100

    wip

--- src/components/AfterStage.jsx
+++ src/components/AfterStage.jsx
@@ -2,7 +2,6 @@ import StageButton from './StageButton.jsx'
 
 const AfterStage = ({ onRestart }) => (
   <div>
-    <h1>After</h1>
     <StageButton onClick={onRestart} label="Play Again" />
   </div>
 )

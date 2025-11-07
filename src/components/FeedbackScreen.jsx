c67fff5cd2a713f202e81551c3ad232f15570ddb
G.J.Hilton <gjhilton@users.noreply.github.com>
2025-11-07 03:47:05 +0000

    placeholder feedbaxk

---
+++ src/components/FeedbackScreen.jsx
@@ -0,0 +1,38 @@
+// post to https://formspree.io/f/xanlrkkz
+
+// Make sure to run npm install @formspree/react
+// For more help visit https://formspr.ee/react-help
+
+import { useForm, ValidationError } from '@formspree/react';
+
+function ContactForm() {
+  const [state, handleSubmit] = useForm("xanlrkkz");
+  if (state.succeeded) {
+      return <p>Thanks for joining!</p>;
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="email">
+        Email Address
+      </label>
+      <input
+        id="email"
+        type="email" 
+        name="email"
+      />
+      <ValidationError 
+        prefix="Email" 
+        field="email"
+        errors={state.errors}
+      />
+     <textarea
+id="message" name="message"
+/>
+‹ValidationError prefix="Message" field="message"
+errors={state.errors}
+/>
+Submit </button>
+</ form>
+‹button type="submit" disabled={state. submitting}>
+) ;
+}

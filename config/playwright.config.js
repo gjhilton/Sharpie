abb35d53f3916330e59addbfeadebc1351f15f92
gjh <guyjohnhilton@gmail.com>
2025-11-09 10:29:42 +0000

    Update playwright.config.js

--- config/playwright.config.js
+++ config/playwright.config.js
@@ -27,7 +27,9 @@ export default defineConfig({
 	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
 	use: {
 		/* Base URL to use in actions like `await page.goto('/')`. */
-		baseURL: 'http://localhost:5175/Sharpie/',
+		baseURL: process.env.CI
+			? 'http://localhost:4173/Sharpie/'
+			: 'http://localhost:5175/Sharpie/',
 
 		/* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
 		trace: 'on-first-retry',
